ef30634659ec6a7d8d915cf579486cb507cd7bfd
piever <pietro.vertechi@neuro.fchampalimaud.org>
2018-03-18 19:57:16 +0000

    use generated function to optimize type checking step

--- src/collect.jl
+++ src/collect.jl
@@ -14,11 +14,11 @@ function collect_to_columns!(dest::Columns{T, U}, itr, offs, st) where {T, U}
     i = offs
     while !done(itr, st)
         el, st = next(itr, st)
-        S = typeof(el)
-        if all((s <: t) for (s, t) in zip(S.parameters, T.parameters))
+        if fieldwise_isa(el, T)
             @inbounds dest[i] = el::T
             i += 1
         else
+            S = typeof(el)
             Rparams = map(typejoin, T.parameters, S.parameters)
             R = get_tuple_type_from_params(el, Rparams)
             new = similar(arrayof(R), length(itr))
@@ -32,3 +32,11 @@ end
 
 get_tuple_type_from_params(el::Tuple, params) = Tuple{params...}
 get_tuple_type_from_params(el::NamedTuple, params) = eval(:(NamedTuples.@NT($(keys(el)...)))){params...}
+
+@generated function fieldwise_isa(el::S, ::Type{T}) where {S, T}
+    if all((s <: t) for (s, t) in zip(S.parameters, T.parameters))
+        return :(true)
+    else
+        return :(false)
+    end
+end
