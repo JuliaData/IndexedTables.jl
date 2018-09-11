a35421711b2c4ee0bf0a8d2922f2d6a246a915ea
quinnj <quinn.jacobd@gmail.com>
2018-09-11 15:19:26 -0600

    Tables.jl cleanup

--- src/tables.jl
+++ src/tables.jl
@@ -1,13 +1,31 @@
 using Tables
 
-Tables.AccessStyle(::Type{<:NextTable}) = Tables.ColumnAccess()
-sch(x::Type{<:NamedTuple}) = x
-sch(x::Type{T}) where {T <: Tuple} = NamedTuple{(Symbol("_$i") for i = 1:length(T.parameters)), T}
-Tables.schema(x::NextTable) = sch(eltype(x))
-Base.getproperty(x::Tuple, i::Int) = getfield(x, i)
-Tables.columns(x::NextTable) = columns(x)
+Tables.istable(::Type{<:NextTable}) = true
+Tables.rowaccess(::Type{<:NextTable}) = true
 Tables.rows(x::NextTable) = rows(x)
+Tables.columnaccess(::Type{<:NextTable}) = true
+Tables.columns(x::NextTable) = columns(x)
 
-table(x; kwargs...) = table(Tables.columntable(x); copy=false, kwargs...)
+sch(x::Type{<:NamedTuple}) = x
+sch(x::Type{T}) where {T <: Tuple} = NamedTuple{(i for i = 1:fieldcount(T)), T}
+Tables.schema(x::NextTable) = Tables.Schema(sch(eltype(x)))
+Base.getproperty(x::Tuple, i::Int) = getfield(x, i)
 
-# IteratorInterfaceExtensions.getiterator(x::NextTable) = Tables.datavalues(Tables.rows(x))
+function table(x::T; kwargs...) where {T}
+    if Tables.istable(T)
+        return table(Tables.columntable(x); copy=false, kwargs...)
+    end
+    y = IteratorInterfaceExtensions.getiterator(x)
+    yT = typeof(y)
+    if Base.isiterable(yT)
+        if Base.IteratorEltype(yT) === Base.HasEltype() && eltype(y) <: NamedTuple
+            return table(Tables.columns(Tables.DataValueUnwrapper(y)); copy=false, kwargs...)
+        else
+            # non-NamedTuple or EltypeUnknown
+            return table(Tables.buildcolumns(nothing, Tables.DataValueUnwrapper(y)); copy=false, kwargs...)
+        end
+    end
+    throw(ArgumentError("unable to construct NextTable from $T"))
+end
+
+IteratorInterfaceExtensions.getiterator(x::NextTable) = Tables.datavaluerows(rows(x))
