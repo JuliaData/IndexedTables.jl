c74f92f43c937388aaa2ed2cd7eb2ddd228e843f
Josh Day <emailjoshday@gmail.com>
2018-12-03 16:13:30 -0500

    uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test, IndexedTables, OnlineStats, DataValues, WeakRefStrings, Tables
 import DataValues: NA
 
 include("test_tables.jl")
-# include("test_core.jl")
-# include("test_utils.jl")
-# include("test_tabletraits.jl")
-# include("test_collect.jl")
+include("test_core.jl")
+include("test_utils.jl")
+include("test_tabletraits.jl")
+include("test_collect.jl")
