493845d784a8bddd6f928386af93a2aaea3afcfc
Josh Day <emailjoshday@gmail.com>
2018-12-04 10:47:52 -0500

    wip

--- src/tables.jl
+++ src/tables.jl
@@ -1,5 +1,6 @@
 #-----------------------------------------------------------------------# Columns 
 const TableColumns = Columns{T} where {T<:NamedTuple}
+Columns(x) = Columns(Tables.columntable(x))
 
 Tables.istable(::Type{<:TableColumns}) = true
 Tables.materializer(c::TableColumns) = Columns
@@ -12,15 +13,23 @@ Tables.columnaccess(c::TableColumns) = true
 Tables.columns(c::TableColumns) = c.columns
 # Tables.schema already defined for NamedTuple of Vectors (c.columns)
 
-#-----------------------------------------------------------------------# IndexedTable/NDSparse
+#-----------------------------------------------------------------------# IndexedTable
 Tables.istable(::Type{IndexedTable{C}}) where {C<:TableColumns} = true
-Tables.istable(::Type{NDSparse{T,D,C,V}}) where {T,D,C<:TableColumns,V<:TableColumns} = true
-
+table(x; kw...) = table(Tables.columntable(x); kw...)
 Tables.materializer(t::IndexedTable) = table
-Tables.materializer(t::NDSparse) = ndpsarse
 
 for f in [:rowaccess, :rows, :columnaccess, :columns, :schema]
-    @eval Tables.$f(t::Dataset) = Tables.$f(Columns(columns(t)))
+    @eval Tables.$f(t::IndexedTable) = Tables.$f(Columns(columns(t)))
 end
+
+#-----------------------------------------------------------------------# NDSparse
+# Tables.istable(::Type{NDSparse{T,D,C,V}}) where {T,D,C<:TableColumns,V<:TableColumns} = true
+# ndsparse(x; kw...) = ndsparse()  # What should be index cols vs. data cols?
+# Tables.materializer(t::NDSparse) = ndpsarse
+
+# for f in [:rowaccess, :rows, :columnaccess, :columns, :schema]
+#     @eval Tables.$f(t::IndexedTable) = Tables.$f(Columns(columns(t)))
+# end
+
 
 
