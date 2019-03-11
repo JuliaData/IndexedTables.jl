bb36c19031e015a76156d715d91e8d3b2b7026ca
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-11 18:12:18 +0000

    simplify and test Tables code

--- test/test_tables.jl
+++ test/test_tables.jl
@@ -8,8 +8,10 @@
     t = table((x=x, y=y, z=z), pkey=[:x, :y])
 
     @test Tables.istable(t)
-    # @test t == table(Tables.rowtable((x=x,y=y,z=z)))
+    @test t == table(Tables.rowtable((x=x,y=y,z=z)))
     @test Tables.istable(columns(t))
     @test Tables.istable(Columns(columns(t)))
+    @test Tables.rowaccess(typeof(t))
+    @test Tables.columnaccess(typeof(t))
     @test Tables.schema(t) == Tables.Schema((:x, :y, :z), (Int, Bool, Float64))
 end
