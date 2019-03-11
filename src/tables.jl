8fb63561be9faba7ddeebf220bddb33eba7c9bbe
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-11 16:29:37 +0000

    define schema for an IndexedTable

--- src/tables.jl
+++ src/tables.jl
@@ -9,8 +9,6 @@ Tables.columns(t::IndexedTable) = Tables.columns(columns(t))
 Tables.rowaccess(::Type{<:IndexedTable}) = true
 Tables.rows(t::IndexedTable) = Tables.rows(rows(t))
 
+Tables.schema(t::IndexedTable) = Tables.Schema(eltype(t))
+
 # table(x; copy=false, kw...) = table(Tables.columntable(x); copy=copy, kw...)
-
-
-
-
